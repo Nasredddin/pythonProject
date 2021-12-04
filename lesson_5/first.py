8454fa29b7b1fc6592315ab4ff02874fa90e80b8
Nasredddin <94569656+Nasredddin@users.noreply.github.com>
2021-12-04 19:34:57 +0200

    Update first.py

--- lesson_5/first.py
+++ lesson_5/first.py
@@ -1,10 +1,13 @@
-enter_num = int(input("Введите число от 3 до 9: \n"))
-if 3 <= enter_num <= 9:
-    for i in range(2, enter_num+2):  # Количество шагов
-        for j in range(1, i - 1):  # Числа по возрастанию
-            print(j, end="")
-        for j in range(i - 1, 0, -1):  # Числа по убыванию
-            print(j, end="")
-        print()
+enter_num = input("Введите число от 3 до 9: \n")
+if enter_num.isdigit():
+    if 3 <= int(enter_num) <= 9:
+        for i in range(2, int(enter_num)+2):  # Количество шагов
+            for j in range(1, i - 1):  # Числа по возрастанию
+                print(j, end="")
+            for j in range(i - 1, 0, -1):  # Числа по убыванию
+                print(j, end="")
+            print()
+    else:
+        print("!")
 else:
-    print("TOTAL ERROR")
+    print("Ошибка. Повторите")
